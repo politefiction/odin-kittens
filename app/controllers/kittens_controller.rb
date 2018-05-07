e4576b6fcc141773a363ce34164302936d172b11
Chelsi Jones <cajones.bz@gmail.com>
2018-05-07 09:01:04 -0500

    Added API functionality

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,6 +2,11 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kittens }
+    end
   end
 
   def new
@@ -10,18 +15,42 @@ class KittensController < ApplicationController
 
   def create
     @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten added!"
+      render 'show'
+    else
+      flash[:danger] = "Error occurred. Please try again."
+      render 'new'
+    end
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kitten }
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "Kitten updated!"
+      render 'show'
+    else
+      flash[:danger] = "Update failed. Try again!"
+      render 'edit'
+    end
   end
 
   def destroy
+    Kitten.find(params[:id]).destroy
+    flash[:success] = "Kitten deleted! How dare you?!"
+    redirect_to root_url
   end
 
   private
